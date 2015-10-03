c25faa5943453e3049b8036a477c0b37609fefa2
Peter Forgacs <foripepe@yahoo.com>
2015-10-03 20:03:14 +0200

    UI development

--- src/GameElements/HexaGrid.js
+++ src/GameElements/HexaGrid.js
@@ -3,6 +3,16 @@
  */
 
 import ui.View;
+import ui.ImageView as ImageView;
+
+/**
+ * Game constants.
+ */
+var hexaGridWidth = 10;
+var hexaGridHeight = 12;
+var hexaGridFilled = 4;
+var ballLength = 16;
+var ballEmpty = 'resources/images/balls/empty.png';
 
 exports = Class(ui.View, function (supr) {
     this.init = function (opts) {
@@ -19,6 +29,32 @@ exports = Class(ui.View, function (supr) {
     };
 
     this.build = function () {
+        // Itaration variables.
+        var i, j;
+
+        // Load the balls.
+        this._balls = [];
+        for (i = 0; i < ballLength; i++) {
+            this._balls[i] = 'resources/images/balls/ball' + i + '.png';
+        }
+
+        // Create the hexagrid.
+        this._hexagrid = [];
+        for (i = 0; i < hexaGridHeight; i++) {
+            this._hexagrid[i] = [];
+            for (j = 0; j < hexaGridWidth; j++) {
+                this._hexagrid[i][j] = new ImageView({
+                    superview: this,
+                    image: ballEmpty,
+                    //this._balls[ Math.floor( Math.random() * ballLength) ]
+                    width: 30,
+                    height: 30,
+                    x: (j * 30) + (i % 2) * 15,
+                    y: (i * 30)
+                });
+            }
+        }
+
         this.on('hexagrid:start', start_game_flow.bind(this));
     };
 });
@@ -27,4 +63,11 @@ exports = Class(ui.View, function (supr) {
  * Game play.
  */
 function start_game_flow () {
+    hexaGridView();
+}
+
+/**
+ * HexaGrid view.
+ */
+function hexaGridView() {
 }
--- src/GameElements/User.js
+++ src/GameElements/User.js
@@ -5,6 +5,12 @@
 import ui.View;
 import ui.ImageView as ImageView;
 
+/**
+ * Game constants.
+ */
+var ballLength = 16;
+var ballEmpty = 'resources/images/balls/empty.png';
+
 exports = Class(ui.View, function (supr) {
     var width = 320;
     var height = 480;
@@ -24,6 +30,13 @@ exports = Class(ui.View, function (supr) {
 
     this.build = function () {
 
+        // Load the balls.
+        this._balls = [];
+        for (var i = 0; i < ballLength; i++) {
+            this._balls[i] = 'resources/images/balls/ball' + i + '.png';
+        }
+
+        // Background
         var bg = new ImageView({
             superview: this,
             image: 'resources/images/bg2.jpg',
@@ -33,13 +46,44 @@ exports = Class(ui.View, function (supr) {
             y: 0
         });
 
-        this._enemyview = new ImageView({
+        // Fired ball
+        this._ball0 = new ImageView({
+            superview: this,
+            image: ballEmpty,
+            width: 30,
+            height: 30,
+            x: (width - 30) / 2,
+            y: height - 45
+        });
+
+        // Cannon
+        this._cannonview = new ImageView({
             superview: this,
             image: 'resources/images/cannon.png',
             width: 60,
             height: 60,
             x: (width - 60) / 2,
             y: height - 60
+        });
+
+        // Cannon ball
+        this._ball1 = new ImageView({
+            superview: this,
+            image: ballEmpty,
+            width: 30,
+            height: 30,
+            x: (width - 30) / 2,
+            y: height - 45
+        });
+
+        // Next ball
+        this._ball2 = new ImageView({
+            superview: this,
+            image: ballEmpty,
+            width: 30,
+            height: 30,
+            x: (width - 120) / 2,
+            y: height - 30
         });
 
         this.on('user:start', start_game_flow.bind(this));
